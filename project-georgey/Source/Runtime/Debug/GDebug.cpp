ebcedc1c9ddf49e93892fa5f875a3b72534f3490
nikhilghosh75 <nighosh@umich.edu>
2020-03-16 11:15:22 -0400

    Created Error and Warning Logging Features. Created Basic Structs

--- project-georgey/Source/Runtime/Debug/GDebug.cpp
+++ project-georgey/Source/Runtime/Debug/GDebug.cpp
@@ -4,12 +4,30 @@
 
 #ifdef console
 #include <iostream>
+#include <windows.h>
 using namespace std;
 #endif
 
 void GDebug::Log(string s)
 {
 #ifdef console
-	cout << s;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	cout << s << endl;
+#endif
+}
+
+void GDebug::LogWarning(string s)
+{
+#ifdef console
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	cout << s << endl;
+#endif
+}
+
+void GDebug::LogError(string s)
+{
+#ifdef console
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	cout << s << endl;
 #endif
 }
--- project-georgey/Source/Runtime/Debug/GDebug.h
+++ project-georgey/Source/Runtime/Debug/GDebug.h
@@ -2,7 +2,23 @@
 #include <string>
 using namespace std;
 
+enum EDebugType
+{
+	Normal,
+	Warning,
+	Error
+};
+
+struct FDebugInfo
+{
+	EDebugType type;
+	string message;
+};
+
 class GDebug
 {
+public:
 	static void Log(string s);
+	static void LogWarning(string s);
+	static void LogError(string s);
 };
