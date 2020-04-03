1a45083f9e684690b3c79a6ce693bae6a40c968d
nikhilghosh75 <nighosh@umich.edu>
2020-04-03 14:47:42 -0400

    Implemented Vertex Layouts for a more modular codebase

--- project-georgey/Source/Runtime/Rendering/Renderer.cpp
+++ project-georgey/Source/Runtime/Rendering/Renderer.cpp
@@ -18,21 +18,14 @@ void Renderer::TempRenderer()
 {
 	GLuint buffer;
 	float positions[6] = { -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f };
-	GDebug::Log("Positions");
 	glGenBuffers(1, &buffer);
-	GDebug::Log("Generate Buffers");
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	GDebug::Log("Bind Buffers");
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-	GDebug::Log("Buffer Data");
 
 	//glClear(GL_COLOR_BUFFER_BIT);
 	glEnableVertexAttribArray(0);
-	GDebug::Log("EnableVertexAtribArray");
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	GDebug::Log("Vertex Attrib Pointer");
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	GDebug::Log("Draw Arrays");
 	glDisableVertexAttribArray(0);
-	GDebug::Log("Disable Vertex Attrib Array");
 }
