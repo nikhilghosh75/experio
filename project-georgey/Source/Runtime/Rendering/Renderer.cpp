bff89d8e1a10fa449b961f8624234900dc63e5c4
nikhilghosh75 <nighosh@umich.edu>
2020-04-06 10:45:45 -0400

    Implemented basic shader functions

--- project-georgey/Source/Runtime/Rendering/Renderer.cpp
+++ project-georgey/Source/Runtime/Rendering/Renderer.cpp
@@ -5,6 +5,44 @@
 #include <GL/GL.h>
 #include "../Debug/GDebug.h"
 
+unsigned int Renderer::CompileShader(const std::string & source, unsigned int type)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = new char[length];
+		glGetShaderInfoLog(id, length, &length, message);
+		GDebug::LogError("Failed to compile shader!" + std::string(message));
+	}
+
+	return id;
+}
+
+unsigned int Renderer::CreateShader(const std::string & vertexShader, const std::string & fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(vertexShader, GL_VERTEX_SHADER);
+	unsigned int fs = CompileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 Renderer::Renderer()
 {
 }
@@ -16,6 +54,7 @@ Renderer::~Renderer()
 
 void Renderer::TempRenderer()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
 	GLuint buffer;
 	float positions[6] = { -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f };
 	glGenBuffers(1, &buffer);
@@ -25,6 +64,28 @@ void Renderer::TempRenderer()
 	//glClear(GL_COLOR_BUFFER_BIT);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
+
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position; \n"
+		"\n"
+		"void main()\n"
+		"{ \n"
+		"    gl_Position = position; \n"
+		"}\n";
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color; \n"
+		"\n"
+		"void main()\n"
+		"{ \n"
+		"    color = vec4(1.0, 0.0, 0.0, 1.0); \n"
+		"}\n";
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
--- project-georgey/Source/Runtime/Rendering/Renderer.h
+++ project-georgey/Source/Runtime/Rendering/Renderer.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "../../Engine.h"
+#include <string>;
 
 class Renderer
 {
+private:
+	unsigned int CompileShader(const std::string& source, unsigned int type);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 public:
 	Renderer();
 	~Renderer();
