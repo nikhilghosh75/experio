4589b90f82b7e9f8483867090e329fda0251e4b8
nikhilghosh75 <nighosh@umich.edu>
2020-04-07 18:40:07 -0400

    Added PowOfTwo function to the Math Library

--- project-georgey/Source/Runtime/Math/LMath.h
+++ project-georgey/Source/Runtime/Math/LMath.h
@@ -7,17 +7,41 @@ class LMath
 public:
 	static float Abs(float x);
 
+	static float Acos(float x);
+
+	static float Asin(float x);
+
+	static float Atan(float x);
+
+	static float Atan2(float x);
+	
+	static int Ceil(float f);
+
+	static float Clamp(float f, float min, float max);
+
+	static float ClampAngleDef(float f);
+
+	static float ClampAngleRad(float f);
+
 	static float Cos(float angle);
 
 	static float DegreesToRadians(float degrees);
 
-	static float Ln(float x);
+	static int Floor(float f);
 
-	static float PowE(float exponent);
+	static float Lerp(float a, float b, float t);
+
+	static float Ln(float x);
 
 	static float Pow(float base, int exponent);
 
+	static float PowE(float exponent);
+
+	static long long PowOfTwo(int e);
+
 	static float RadiansToDegrees(float radians);
+
+	static int Round(float f);
 
 	static float Sin(float angle);
 
