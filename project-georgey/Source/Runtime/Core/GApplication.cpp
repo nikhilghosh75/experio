a6a6aeb7442858539e484046a86f8df20a3382de
nikhilghosh75 <nighosh@umich.edu>
2020-05-06 19:03:45 -0400

    Implemented Test for basic rendering

--- project-georgey/Source/Runtime/Core/GApplication.cpp
+++ project-georgey/Source/Runtime/Core/GApplication.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include "../Debug/TempProfiler.h"
 #include "../Rendering/Renderer.h"
+#include "../Math/FVector3.h"
+#include "../Math/FQuaternion.h"
+#include "../Rendering/MeshData.h"
+#include "../Rendering/FCameraData.h"
 using namespace std;
 
 GApplication::GApplication()
@@ -21,10 +25,17 @@ void GApplication::Run()
 	newWindow.InstantiateWindow();
 	delete windowProfiler;
 	Renderer tempRenderer;
+
+	FMeshData* cubeData = tempRenderer.TempSetup();
+	FCameraData camera(FVector3(4.f, 3.f, -3.f), FQuaternion(glm::lookAt(glm::vec3(4, 3, -3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0))), 45.f);
+
 	while (newWindow.isActive)
 	{
+		tempRenderer.OnNewFrame();
 		newWindow.OnUpdate();
-		tempRenderer.TempRenderer();
+		//tempRenderer.TempRenderer();
+		tempRenderer.DrawMesh(*cubeData, camera);
 	}
 	cin.get();
+	delete cubeData;
 }
