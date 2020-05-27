50b19820cfc299578fc6b406b6ac6de9665a8386
nikhilghosh75 <nighosh@umich.edu>
2020-05-26 20:45:43 -0400

    Created EWindowAxisType

--- project-georgey/Source/Runtime/Core/WindowEnums.h
+++ project-georgey/Source/Runtime/Core/WindowEnums.h
@@ -2,10 +2,17 @@
 
 // Enums specific to windows
 
-enum EWindowResizeType
+enum class EWindowResizeType
 {
 	Resize = 0, // Normal Resize
 	Minimize = 1, // When Minimized is clicked
 	Maximize = 2, // When Maximized is clicked
 	Create = 3 // When the window is created
+};
+
+enum class EWindowAxisType
+{
+	None = 0,
+	X = 1,
+	Y = 2
 };
