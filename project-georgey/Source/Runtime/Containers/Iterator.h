b96ebf6ae86ab0ed2dd592956973a9139bb9b05e
nikhilghosh75 <nighosh@umich.edu>
2020-08-05 09:50:02 -0400

    Fixed public inheritance bug on Interators

--- project-georgey/Source/Runtime/Containers/Iterator.h
+++ project-georgey/Source/Runtime/Containers/Iterator.h
@@ -7,39 +7,39 @@ public:
 	Container& container;
 	Element& current;
 
-	operator==(const TIterator<Container, Element>& other)
+	bool operator==(const TIterator<Container, Element>& other)
 	{
 		return current == other.current && container == other.container;
 	}
 
-	operator!=(const TIterator<Container, Element>& other)
+	bool operator!=(const TIterator<Container, Element>& other)
 	{
 		return current != other.current || container != other.container;
 	}
 };
 
 template<class Container, class Element>
-class TForwardIterator : TIterator<Container, Element>
+class TForwardIterator : public TIterator<Container, Element>
 {
 public:
 	virtual bool IsAtEnd() { return false; }
 
-	virtual operator++() {}
+	virtual void Increment() {}
 };
 
 template <class Container, class Element>
-class TBidirectionalIterator : TForwardIterator<Container, Element>
+class TBidirectionalIterator : public TForwardIterator<Container, Element>
 {
 public:
 	virtual bool IsAtBegining() { return false; }
 
-	virtual operator--() {};
+	virtual TBidirectionalIterator<Container, Element> operator--() {};
 };
 
 template<class Container, class Element>
-class TRandomAccessIterator : TBidirectionalIterator<Container, Element>
+class TRandomAccessIterator : public TBidirectionalIterator<Container, Element>
 {
 public:
-	virtual operator+=(const int n) {};
-	virtual operator-=(const int n) {};
+	virtual TRandomAccessIterator<Container, Element> operator+=(const int n) {};
+	virtual TRandomAccessIterator<Container, Element> operator-=(const int n) {};
 };
