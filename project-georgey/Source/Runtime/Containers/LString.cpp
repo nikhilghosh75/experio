60161fcb36fae4493c362b724c67813e0494763f
nikhilghosh75 <nighosh@umich.edu>
2020-08-20 21:47:16 -0400

    Added Char functions and safer StringToInt and StringToFloat functions

--- project-georgey/Source/Runtime/Containers/LString.cpp
+++ project-georgey/Source/Runtime/Containers/LString.cpp
@@ -32,6 +32,55 @@ std::vector<std::string> LString::SeperateStringByChar(std::string str, char c, 
 	return realSeperatedStrings;
 }
 
+float LString::StringToFloat(std::string str)
+{
+	float integer = 0.f, fraction = 0.f;
+	uint8_t denominator = 0;
+	bool beforeDecimal = true, isPositive = true;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '-')
+		{
+			isPositive = false;
+		}
+		else if (str[i] == '.')
+		{
+			beforeDecimal = false;
+		}
+		else if (IsNumeric(str[i]))
+		{
+			if (beforeDecimal) 
+			{ 
+				integer = (integer * 10) + CharToInt(str[i]);
+			}
+			else
+			{
+				fraction = (fraction * 10) + CharToInt(str[i]);
+				denominator++;
+			}
+		}
+	}
+	return isPositive ? integer + (fraction / (float)LMath::PowOfTen(denominator)) : (integer + (fraction / (float)LMath::PowOfTen(denominator))) * -1.f;
+}
+
+int LString::StringToInt(std::string str)
+{
+	int integer = 0;
+	bool isPositive = true;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '-')
+		{
+			isPositive = false;
+		}
+		else if (IsNumeric(str[i]))
+		{
+			integer = (integer * 10) + CharToInt(str[i]);
+		}
+	}
+	return isPositive ? integer : integer * -1;
+}
+
 unsigned int LString::HexStringToInt(std::string s)
 {
 	unsigned int returnValue = 0;
@@ -59,6 +108,36 @@ unsigned int LString::HexCharToInt(char c)
 		return c - 97 + 10;
 	}
 	return 0;
+}
+
+bool LString::IsAlpha(char c)
+{
+	return (c > 64 && c < 91) || (c > 96 && c < 121);
+}
+
+bool LString::IsAlphaNumeric(char c)
+{
+	return IsAlpha(c) || IsNumeric(c);
+}
+
+bool LString::IsLower(char c)
+{
+	return c > 96 && c < 121;
+}
+
+bool LString::IsNumeric(char c)
+{
+	return c > 47 && c < 58;
+}
+
+bool LString::IsUpper(char c)
+{
+	return c > 64 && c < 91;
+}
+
+int LString::CharToInt(char c)
+{
+	return c - 48;
 }
 
 std::string LString::GetFileLocation(const std::string fileName)
--- project-georgey/Source/Runtime/Containers/LString.h
+++ project-georgey/Source/Runtime/Containers/LString.h
@@ -6,13 +6,25 @@
 class LString
 {
 public:
-	static std::vector<std::string> SeperateStringByChar(std::string str, char c, bool removeZeroLength = true);
+	static int CharToInt(char c);
+
+	static std::string GetFileLocation(const std::string fileName);
+
+	static std::string GetFileName(const std::string fileName);
 
 	static unsigned int HexStringToInt(std::string s);
 
 	static unsigned int HexCharToInt(char c);
 
-	static std::string GetFileLocation(const std::string fileName);
+	static bool IsAlpha(char c);
+	static bool IsAlphaNumeric(char c);
+	static bool IsLower(char c);
+	static bool IsNumeric(char c);
+	static bool IsUpper(char c);
 
-	static std::string GetFileName(const std::string fileName);
+	static std::vector<std::string> SeperateStringByChar(std::string str, char c, bool removeZeroLength = true);
+
+	static float StringToFloat(std::string str);
+
+	static int StringToInt(std::string str);
 };
