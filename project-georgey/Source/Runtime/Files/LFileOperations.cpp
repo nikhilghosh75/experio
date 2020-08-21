1e4ead0f6a44f425143b0cc6cba6827090d4d2f5
nikhilghosh75 <nighosh@umich.edu>
2020-08-20 21:29:13 -0400

    Added GetExtension and GetFullFilePath functions

--- project-georgey/Source/Runtime/Files/LFileOperations.cpp
+++ project-georgey/Source/Runtime/Files/LFileOperations.cpp
@@ -15,3 +15,31 @@ bool LFileOperations::DoesFileHaveExtension(std::string filePath, const char * e
 
 	return strcmp(&filePath[indexOfDot + 1], extension) == 0;
 }
+
+std::string LFileOperations::GetExtension(std::string filePath)
+{
+	int indexOfDot = 0;
+
+	for (int i = filePath.size() - 2; i >= 2; i--)
+	{
+		if (filePath[i] == '.')
+		{
+			indexOfDot = i;
+			break;
+		}
+	}
+
+	return filePath.substr(indexOfDot + 1);
+}
+
+std::string LFileOperations::GetFullFilePath(std::string filePath)
+{
+	if (filePath[0] == '?' || filePath[1] == '?')
+	{
+		if (filePath.find("?Standard?") != std::string::npos)
+		{
+			return "C:/Users/debgh/source/repos/project-bloo/project-georgey/Resources/Standard/" + filePath.substr(11);
+		}
+	}
+	return "C:/Users/debgh/source/repos/project-bloo/" + filePath;
+}
--- project-georgey/Source/Runtime/Files/LFileOperations.h
+++ project-georgey/Source/Runtime/Files/LFileOperations.h
@@ -5,4 +5,8 @@ class LFileOperations
 {
 public:
 	static bool DoesFileHaveExtension(std::string filePath, const char* extension);
+
+	static std::string GetExtension(std::string filePath);
+
+	static std::string GetFullFilePath(std::string filePath);
 };
