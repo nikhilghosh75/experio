e9a23c29a0ee943de8fc9aaa4afd7dbe5798af60
nikhilghosh75 <nighosh@umich.edu>
2020-09-03 15:49:24 -0400

    Created MaterialManager class

---
+++ Experio/Source/Runtime/Rendering/Materials/MaterialManager.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "Material.h"
+#include "../../Files/LFileOperations.h"
+#include "../../Debug/Debug.h"
+#include <string>
+#include <vector>
+#include <fstream>
+
+class MaterialManager
+{
+public:
+	Material* LoadMaterialFromFile(std::string filePath)
+	{
+		if (!LFileOperations::DoesFileHaveExtension(filePath, "material"))
+		{
+			Debug::LogError("Loaded Material is not a .material file");
+			return nullptr;
+		}
+
+		ifstream materialFile(filePath);
+		if (materialFile.fail())
+		{
+			Debug::LogError("File " + filePath + " could not be opened");
+			return nullptr;
+		}
+
+		char word[256];
+		materialFile.getline(word, 256);
+		
+		std::string materialType;
+		std::string shader;
+		std::vector<std::string> params;
+
+		materialFile >> word;
+		materialFile.getline(word, 256);
+		materialType = std::string(word);
+
+		materialFile >> word;
+		materialFile.getline(word, 256);
+		shader = std::string(word);
+
+		while (materialFile >> word)
+		{
+			materialFile.getline(word, 256);
+			params.push_back(std::string(word));
+		}
+
+		return AddMaterial(materialType, shader, params);
+	}
+
+	virtual Material* AddMaterial(std::string materialType, std::string shader, std::vector<std::string> params) { return nullptr; }
+};
