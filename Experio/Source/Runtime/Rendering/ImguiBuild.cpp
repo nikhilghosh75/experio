c203fea4a3760b4d4d7f21489593eaa4c674dc96
nikhilghosh75 <nighosh@umich.edu>
2020-09-11 16:27:13 -0400

    Fixed ImGui Build

---
+++ Experio/Source/Runtime/Rendering/ImguiBuild.cpp
@@ -0,0 +1,16 @@
+#define IMGUI_IMPL_OPENGL_LOADER_GLEW
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#ifdef PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
+
+#include "imgui.h"
+#include "imgui_internal.h";
+
+#include "imgui.cpp";
+#include "imgui_demo.cpp"
+#include "imgui_draw.cpp";
+#include "imgui_widgets.cpp";
+#include "examples/imgui_impl_opengl3.cpp";
+#include "examples/imgui_impl_win32.cpp";
