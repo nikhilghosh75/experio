9e2b2f6f6fa9897af0420b075e77363a727d3633
nikhilghosh75 <nighosh@umich.edu>
2020-09-19 22:55:19 -0400

    Created GUID class

---
+++ Experio/Source/Runtime/Framework/GUID.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <stdint.h>
+#include <string>
+#include "../Containers/LString.h"
+
+struct GUID
+{
+	uint64_t id;
+
+	GUID(uint64_t id)
+	{
+		this->id = id;
+	}
+
+	GUID(std::string s)
+	{
+		this->id = (uint64_t)LString::HexStringToLong(s);
+	}
+
+	std::string ToString()
+	{
+
+	}
+
+	bool operator==(const GUID& other)
+	{
+		return this->id == other.id;
+	}
+
+	bool operator!=(const GUID& other)
+	{
+		return this->id != other.id;
+	}
+};
