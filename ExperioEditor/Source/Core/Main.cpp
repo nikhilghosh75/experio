4d4c38251c6804f930d71db07cfc65403205187f
nikhilghosh75 <nighosh@umich.edu>
2020-09-21 15:30:57 -0400

    Restructured Editor loop

--- ExperioEditor/Source/Core/Main.cpp
+++ ExperioEditor/Source/Core/Main.cpp
@@ -1,35 +1,7 @@
-#include <Windows.h>
-#include <stdio.h>
-#include "GL/glew.h"
-#include <gl/GL.h>
-#include "GL/wglext.h"
-#include <tchar.h>
-#include "imgui.h"
-#include "examples/imgui_impl_win32.h"
-#include "examples/imgui_impl_opengl3.h"
-#include "imgui_internal.h"
-
-#include "EditorWindow.h"
-#include "Runtime/Core/Window.h"
+#include "EditorApplication.h"
 
 int main(int argc, char** argv)
 {
-	EditorWindow::InitializeWindow();
-
-	while (EditorWindow::isActive)
-	{
-		EditorWindow::BeginFrame();
-
-		ImGui::Begin("Test");
-		ImGui::Text("Hello World");
-		ImGui::End();
-
-		ImGui::Begin("Test 2");
-		ImGui::Text("Hello Second World");
-		ImGui::End();
-
-		EditorWindow::EndFrame();
-	}
-	
-	EditorWindow::CloseWindow();
+	EditorApplication application;
+	application.Run();
 }
