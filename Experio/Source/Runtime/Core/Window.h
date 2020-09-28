a04d71dcea9665be951d52df239039c7673edad6
nikhilghosh75 <nighosh@umich.edu>
2020-09-27 22:31:08 -0400

    Added Editor Resize mode for Window

--- Experio/Source/Runtime/Core/Window.h
+++ Experio/Source/Runtime/Core/Window.h
@@ -21,7 +21,8 @@ enum class EWindowResizeType
 	Resize = 0, // Normal Resize
 	Minimize = 1, // When Minimized is clicked
 	Maximize = 2, // When Maximized is clicked
-	Create = 3 // When the window is created
+	Create = 3, // When the window is created
+	EditorResize = 8 // When Editor Window is Resized
 };
 
 enum class EWindowAxisType
