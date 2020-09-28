33594b1e5a7814ab63691677ef3ee4b37cec406c
nikhilghosh75 <nighosh@umich.edu>
2020-09-27 22:26:15 -0400

    Added temporary editor loop (may delete later)

--- ExperioEditor/Source/Core/EditorApplication.cpp
+++ ExperioEditor/Source/Core/EditorApplication.cpp
@@ -12,13 +12,16 @@
 
 #include "EditorWindow.h"
 #include "Runtime/Core/Window.h"
+#include "../Framework/ValueLoader.h"
 #include "../Test Modules/TestModule.h"
 #include "Runtime/Framework/SceneLoader.h"
 #include "Runtime/Framework/Project.h"
+#include "../Framework/EditorProject.h"
 
-typedef void(__cdecl *MYPROC)();
+std::string projectName = "Experio Editor";
 
 std::vector<EditorModule*> EditorApplication::modules;
+DllLoader EditorApplication::loader;
 
 EditorApplication::EditorApplication()
 {
@@ -40,38 +43,36 @@ void EditorApplication::Run()
 	LoadProject("C:/Users/debgh/source/repos/project-bloo/bin/x64/Debug/Demo Project/Demo Project.dll");
 	modules.push_back(new TestModule());
 
+	EditorProject::TempSetup();
+	bool sceneLoadedSuccess;
+	// loader.CallFunction<bool, BOOLSTRINGINTPROC, std::string, int>("LoadScene", sceneLoadedSuccess, "C:/Users/debgh/source/repos/project-bloo/Demo Project/Assets/Scenes/TestScene.pbscene", 0);
+
 	while (EditorWindow::isActive)
 	{
 		EditorWindow::BeginFrame();
+		// loader.CallFunction("Update");
 		RenderModules();
 		EditorWindow::EndFrame();
 	}
 
 	EditorWindow::CloseWindow();
+
+	loader.UnloadDll();
 }
 
 void EditorApplication::LoadProject(std::string dllFilePath)
 {
-	HINSTANCE hinstLib;
-	MYPROC ProcSetup, ProcRun;
-
-	hinstLib = LoadLibrary(TEXT(dllFilePath.c_str()));
-
-	if (hinstLib != NULL)
-	{
-		ProcSetup = (MYPROC)GetProcAddress(hinstLib, "SetupProject");
-
-		if (NULL != ProcSetup)
-		{
-			(ProcSetup)();
-		}
-	}
+	loader.LoadDll(dllFilePath);
+	loader.CallFunction("SetupProject");
+	loader.CallVoidFunction<VOIDHWNDPROC, HWND>("SetupGraphics", EditorWindow::GetHWND());
 }
 
 void EditorApplication::RenderModules()
 {
 	for (int i = 0; i < modules.size(); i++)
 	{
+		ImGui::Begin(("Module " + std::to_string(i)).c_str());
 		modules[i]->Display();
+		ImGui::End();
 	}
 }
--- ExperioEditor/Source/Core/EditorApplication.h
+++ ExperioEditor/Source/Core/EditorApplication.h
@@ -2,11 +2,14 @@
 #include <vector>
 #include <string>
 #include "EditorModule.h"
+#include "Runtime/Core/DllLoader.h"
 
 class EditorApplication
 {
 	static std::vector<EditorModule*> modules;
 public:
+	static DllLoader loader;
+
 	EditorApplication();
 	~EditorApplication();
 
