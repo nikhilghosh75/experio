52cf1a0106f9e07bda9087ad55ed2117351403c8
nikhilghosh75 <nighosh@umich.edu>
2020-09-27 22:23:56 -0400

    Fixed DemoProjectExe

--- Demo Project/Generated/Demo Project_Exe/Main.cpp
+++ Demo Project/Generated/Demo Project_Exe/Main.cpp
@@ -14,36 +14,3 @@ int main(int argc, char** argv)
 	RunProject();
 	return 0;
 }
-
-/*
-	HINSTANCE hinstLib;
-	MYPROC ProcSetup, ProcRun;
-
-	hinstLib = LoadLibrary(TEXT("Demo Project.dll"));
-
-	if (hinstLib != NULL)
-	{
-		ProcSetup = (MYPROC)GetProcAddress(hinstLib, "SetupProject");
-
-		if (NULL != ProcSetup)
-		{
-			(ProcSetup)();
-			ProcRun = (MYPROC)GetProcAddress(hinstLib, "RunProject");
-			if (ProcRun != NULL)
-			{
-				(ProcRun)();
-			}
-		}
-		else
-		{
-			auto error = GetLastError();
-			printf("%i\n", error);
-		}
-
-		FreeLibrary(hinstLib);
-	}
-	else
-	{
-		printf("Linking failed");
-	}
-*/
