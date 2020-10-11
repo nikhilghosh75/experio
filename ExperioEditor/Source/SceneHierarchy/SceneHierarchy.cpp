227c20147205eaf4853a94e27e170244d198309e
nikhilghosh75 <nighosh@umich.edu>
2020-10-11 10:41:19 -0400

    Added SceneHierarchy Multiselect and Drag/Drop

--- ExperioEditor/Source/SceneHierarchy/SceneHierarchy.cpp
+++ ExperioEditor/Source/SceneHierarchy/SceneHierarchy.cpp
@@ -2,6 +2,110 @@
 #include "Runtime/Framework/GameObject.h"
 #include "Runtime/Rendering/LImGui.h"
 #include "Runtime/Framework/Scene.h"
+#include "Runtime/Containers/LStandard.h"
+
+void SceneHierarchy::HandleDragDrop(const GameObject * gameObject)
+{
+	if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
+	{
+		uint64_t id = gameObject->id;
+		ImGui::SetDragDropPayload("EXPERIO_GAME_OBJECT", &id, sizeof(uint64_t));
+		ImGui::EndDragDropSource();
+	}
+	if (ImGui::BeginDragDropTarget())
+	{
+		const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("EXPERIO_GAME_OBJECT");
+		if (payload != nullptr)
+		{
+			uint64_t id = *(const uint64_t*)payload->Data;
+			HandleSwap(id, gameObject->id);
+		}
+		Debug::Log("Payload not accepted");
+		ImGui::EndDragDropTarget();
+	}
+}
+
+void SceneHierarchy::HandleSwap(uint64_t fromID, uint64_t toID)
+{
+	// Debug::log << "Swap " << fromID << ", " << toID << Debug::endl;
+	if (fromID == toID)
+	{
+		return;
+	}
+
+	GameObject* fromObject = Scene::FindGameObjectFromId(fromID);
+	GameObject* toObject = Scene::FindGameObjectFromId(toID);
+
+	// Same Parent
+	if (fromObject->parent->id == toObject->parent->id)
+	{
+		GameObject* parentObject = fromObject->parent;
+		unsigned int fromIndex, toIndex;
+		for (unsigned int i = 0; i < parentObject->children.size(); i++)
+		{
+			if (parentObject->children[i]->id == fromID)
+			{
+				fromIndex = i;
+			}
+			if (parentObject->children[i]->id == toID)
+			{
+				toIndex = i;
+			}
+		}
+
+		// Swap
+		parentObject->children[fromIndex] = toObject;
+		parentObject->children[toIndex] = fromObject;
+	}
+
+	// TO-DO: Add other types here
+}
+
+void SceneHierarchy::DisplayGameObjectTree(const GameObject * gameObject, std::vector<GameObject>& selectedItems)
+{
+	bool isSelected = LStandard::ExistsInVector(selectedItems, *gameObject);
+	if (gameObject->children.size() == 0)
+	{
+		if (ImGui::Selectable(gameObject->name.c_str(), isSelected))
+		{
+			if (!isSelected)
+			{
+				selectedItems.push_back(*gameObject);
+			}
+			else
+			{
+				LStandard::RemoveElement(selectedItems, *gameObject);
+			}
+		}
+		HandleDragDrop(gameObject);
+	}
+	else
+	{
+		ImGuiTreeNodeFlags_ flags = isSelected ? ImGuiTreeNodeFlags_Selected : ImGuiTreeNodeFlags_None;
+		if (ImGui::TreeNodeEx(gameObject->name.c_str(), flags))
+		{
+			if (ImGui::IsItemClicked())
+			{
+				if (isSelected)
+				{
+					LStandard::RemoveElement(selectedItems, *gameObject);
+				}
+				else
+				{
+					selectedItems.push_back(*gameObject);
+				}
+			}
+
+			HandleDragDrop(gameObject);
+
+			for (int i = 0; i < gameObject->children.size(); i++)
+			{
+				DisplayGameObjectTree(gameObject->children[i], selectedItems);
+			}
+			ImGui::TreePop();
+		}
+	}
+}
 
 SceneHierarchy::SceneHierarchy()
 {
@@ -11,25 +115,9 @@ SceneHierarchy::SceneHierarchy()
 
 void SceneHierarchy::Display()
 {
-	std::string& selectedItem = this->currentlySelectedItem;
+	std::vector<GameObject>& selectedItems = this->currentlySelectedItems;
 
-	Scene::ForAllActiveScenes([&selectedItem](Scene scene) {
-		TTypedTree<GameObject>* hierarchy = GameObject::MakeTree(scene.GetId());
-		TTypedTree<std::string>* hierarchyNames = new TTypedTree<std::string>();
-		MakeTreeFromBase<GameObject, std::string>(hierarchy, hierarchyNames, [](GameObject object) { return object.name; });
-		LImGui::DisplayTree(hierarchyNames, scene.GetName(), selectedItem);
-		
-		if (selectedItem.size() == 0) return;
-
-		TTypedTreeNode<GameObject>* selectedObjectNode = SearchCorrespondingTrees<std::string, GameObject>
-			(hierarchyNames, hierarchy, [&selectedItem](std::string str) { return str == selectedItem; });
-
-		if (selectedObjectNode != nullptr)
-		{
-			Debug::Log(selectedObjectNode->object.name);
-		}
-
-		delete hierarchy;
-		delete hierarchyNames;
+	Scene::ForAllActiveScenes([&selectedItems](Scene scene) {
+		SceneHierarchy::DisplayGameObjectTree(&scene.sceneRoot, selectedItems);
 	});
 }
--- ExperioEditor/Source/SceneHierarchy/SceneHierarchy.h
+++ ExperioEditor/Source/SceneHierarchy/SceneHierarchy.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "../Core/EditorModule.h"
+#include "Runtime/Framework/GameObject.h"
 
 class SceneHierarchy : public EditorModule
 {
-	std::string currentlySelectedItem;
+	std::vector<GameObject> currentlySelectedItems;
+
+	static void HandleDragDrop(const GameObject* gameObject);
+	static void HandleSwap(uint64_t fromID, uint64_t toID);
+
 public:
+	static void DisplayGameObjectTree(const GameObject* gameObject, std::vector<GameObject>& selectedItems);
+
 	SceneHierarchy();
 
 	virtual void Display() override;
