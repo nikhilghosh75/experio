f1f7308d12690a9c7825e52c3328c9950a700c2d
nikhilghosh75 <nighosh@umich.edu>
2020-10-19 15:35:22 -0400

    Created FVector2Int

---
+++ Experio/Source/Runtime/Math/FVector2Int.cpp
@@ -0,0 +1,13 @@
+#include "FVector2Int.h"
+
+FVector2Int::FVector2Int()
+{
+	this->x = 0;
+	this->y = 0;
+}
+
+FVector2Int::FVector2Int(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
---
+++ Experio/Source/Runtime/Math/FVector2Int.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct FVector2Int
+{
+	int x, y;
+
+	FVector2Int();
+	FVector2Int(int x, int y);
+};
