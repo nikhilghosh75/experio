dd8a8b946929b9542e9e189c261972070bcbdd4d
nikhilghosh75 <nighosh@umich.edu>
2020-10-21 12:36:52 -0400

    Created Math.h for anyone who wants to include all math code

---
+++ Experio/Source/Runtime/Math/Math.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "LMath.h"
+#include "FBox.h"
+#include "FColor.h"
+#include "FCurve.h"
+#include "FQuaternion.h"
+#include "FRay.h"
+#include "FSphericalPoint.h"
+#include "FTransform.h"
+#include "FVector2.h"
+#include "FVector2Int.h"
+#include "FVector3.h"
+#include "FVector4.h"
