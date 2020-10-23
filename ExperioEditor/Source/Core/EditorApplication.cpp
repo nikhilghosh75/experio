bfe1f3a3d37206eff5f98e6a249c08043ceaf67f
nikhilghosh75 <nighosh@umich.edu>
2020-10-22 21:07:57 -0400

    Restructured EditorApplication

--- ExperioEditor/Source/Core/EditorApplication.cpp
+++ ExperioEditor/Source/Core/EditorApplication.cpp
@@ -5,19 +5,22 @@
 #include "GL/wglext.h"
 #include <tchar.h>
 #include "imgui.h"
+#include "imgui_demo.h"
 #include "examples/imgui_impl_win32.h"
 #include "examples/imgui_impl_opengl3.h"
 #include "imgui_internal.h"
 
 #include "EditorWindow.h"
 #include "../Framework/ValueLoader.h"
-#include "../Test Modules/TestModule.h"
 #include "Runtime/Framework/SceneLoader.h"
 #include "../Framework/EditorProject.h"
 #include "../SceneView/SceneView.h"
 #include "../FileView/FileView.h"
 #include "../SceneHierarchy/SceneHierarchy.h"
 #include "Runtime/Debug/TempProfiler.h"
+#include "../CodeParser/CodeProject.h"
+#include "../CodeParser/CodeProjectGenerator.h"
+#include "../Inspector/Inspector.h"
 
 std::vector<EditorModule*> EditorApplication::modules;
 DllLoader EditorApplication::loader;
@@ -34,33 +37,55 @@ EditorApplication::~EditorApplication()
 	}
 }
 
-void EditorApplication::Run()
+void EditorApplication::Setup()
 {
 	EditorWindow::InitializeWindow();
 	Project::inEditor = true;
 
-	// modules.push_back(new TestModule());
 	modules.push_back(new SceneView());
 	modules.push_back(new FileView());
 	modules.push_back(new SceneHierarchy());
+	modules.push_back(new Inspector());
 
 	EditorProject::TempSetup();
 	SceneLoader::LoadSceneFromFile("C:/Users/debgh/source/repos/project-bloo/Demo Project/Assets/Scenes/TestScene.pbscene", 0);
 	Scene::Activate(0);
 
 	Project::StartGame();
+}
+
+void EditorApplication::Run()
+{
+	TempProfiler* profiler = new TempProfiler("Code Parsers");
+	FCodeProjectOptions options;
+	options.codingLanguage = ECodingLanguage::CPlusPlus;
+	options.generateInstantly = true;
+	CodeProject project("C:/Users/debgh/source/repos/project-bloo/Experio/Source/Runtime", options);
+	delete profiler;
 
 	while (EditorWindow::isActive)
 	{
 		PROFILE_SCOPE("Editor Loop");
-		EditorWindow::BeginFrame();
-		Project::BeginFrame();
+		
+		BeginFrame();
 		RenderModules();
+		// bool temp = true;
+		// ImGui::ShowDemoWindow(&temp);
+		EndFrame();
 
-		EditorWindow::EndFrame();
 	}
 
 	EditorWindow::CloseWindow();
+}
+
+void EditorApplication::SetBeginFrameCallback(void(*callback)(float))
+{
+	beginFrameCallback = callback;
+}
+
+void EditorApplication::SetEndFrameCallback(void(*callback)(float))
+{
+	endFrameCallback = callback;
 }
 
 void EditorApplication::LoadProject(std::string dllFilePath)
@@ -68,6 +93,27 @@ void EditorApplication::LoadProject(std::string dllFilePath)
 	loader.LoadDll(dllFilePath);
 	loader.CallFunction("SetupProject");
 	loader.CallVoidFunction<VOIDHWNDPROC, HWND>("SetupGraphics", EditorWindow::GetHWND());
+}
+
+void EditorApplication::BeginFrame()
+{
+	EditorWindow::BeginFrame();
+	Project::BeginFrame();
+
+	if (beginFrameCallback != nullptr)
+	{
+		beginFrameCallback(GameTime::deltaTime);
+	}
+}
+
+void EditorApplication::EndFrame()
+{
+	if (endFrameCallback != nullptr)
+	{
+		endFrameCallback(GameTime::deltaTime);
+	}
+
+	EditorWindow::EndFrame();
 }
 
 void EditorApplication::RenderModules()
--- ExperioEditor/Source/Core/EditorApplication.h
+++ ExperioEditor/Source/Core/EditorApplication.h
@@ -7,16 +7,27 @@
 class EditorApplication
 {
 	static std::vector<EditorModule*> modules;
+
+	void(*beginFrameCallback)(float);
+	void(*endFrameCallback)(float);
 public:
 	static DllLoader loader;
 
 	EditorApplication();
 	~EditorApplication();
 
+	void Setup();
+
 	void Run();
+
+	void SetBeginFrameCallback(void(*callback)(float));
+	void SetEndFrameCallback(void(*callback)(float));
 
 	void LoadProject(std::string dllFilePath); // Change Later
 
 private:
+	void BeginFrame();
+	void EndFrame();
+
 	void RenderModules();
 };
