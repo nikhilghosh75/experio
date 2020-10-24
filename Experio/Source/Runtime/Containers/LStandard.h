c896de2f3689c64569b08398563a6a73aace5e2c
nikhilghosh75 <nighosh@umich.edu>
2020-10-23 23:02:25 -0400

    Added functions for vectors of pointers

--- Experio/Source/Runtime/Containers/LStandard.h
+++ Experio/Source/Runtime/Containers/LStandard.h
@@ -23,8 +23,39 @@ public:
 		return false;
 	}
 
+	template<typename T>	
+	static bool ExistsInVector(const std::vector<T*>& vector, const T* object)
+	{
+		for (int i = 0; i < vector.size(); i++)
+		{
+			if (vector[i] == object)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
 	template<typename T>
 	static void RemoveElement(std::vector<T>& vector, const T& object)
+	{
+		bool found = false;
+		for (int i = 0; i < vector.size(); i++)
+		{
+			if (vector[i] == object)
+			{
+				found = true;
+			}
+			else if (found)
+			{
+				vector[i - 1] = vector[i];
+			}
+		}
+		vector.pop_back();
+	}
+
+	template<typename T>
+	static void RemoveElement(std::vector<T*>& vector, const T* object)
 	{
 		bool found = false;
 		for (int i = 0; i < vector.size(); i++)
