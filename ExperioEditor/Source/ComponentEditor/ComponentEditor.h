7cc3a726c9adf5a43320467140ad565d91f1a110
nikhilghosh75 <nighosh@umich.edu>
2020-10-27 18:21:14 -0400

    Created base componentEditor

---
+++ ExperioEditor/Source/ComponentEditor/ComponentEditor.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Runtime/Framework/Component.h"
+#include "Runtime/Framework/Project.h"
+
+class ComponentEditorBase
+{
+public:
+	virtual void OnComponentAdd(Component* c) {};
+
+	virtual void OnComponentOpen(Component* c) {};
+
+	virtual void Display(Component* c) = 0;
+
+	virtual unsigned int GetComponentID() const { return 0; }
+};
+
+template <typename T>
+class ComponentEditor {};
+
+#define COMPONENTEDITOR(_type_) public ComponentEditor<_type_>, public ComponentEditorBase
+
+#define SETUP_COMPONENT_EDITOR(_type_) \
+virtual unsigned int GetComponentID() const override { return Project::ClassTypeToInt<_type_>(); }
