4b067098f7f4dfc1df5e7c2deb44b17a534088f1
nikhilghosh75 <nighosh@umich.edu>
2020-10-29 19:36:59 -0400

    Finished SceneSaver script

--- ExperioEditor/Source/Framework/SceneSaver.cpp
+++ ExperioEditor/Source/Framework/SceneSaver.cpp
@@ -1,6 +1,59 @@
 #include "SceneSaver.h"
 #include "Runtime/Framework/Framework.h"
-#include <fstream>
+
+extern void SaveComponentToScene(unsigned int componentID, Component* component, std::ofstream& stream);
+
+void SceneSaver::SaveGameObject(GameObject * gameObject, std::ofstream& stream)
+{
+	stream << "\tName: " << gameObject->name << std::endl;
+	stream << "\tTransform: ";
+	stream << gameObject->localPosition.x << " " << gameObject->localPosition.y << " " << gameObject->localPosition.z << " ";
+	stream << gameObject->localRotation.x << " " << gameObject->localRotation.y << " " << gameObject->localRotation.z << " " << gameObject->localRotation.w << " ";
+	stream << gameObject->localScale.x << " " << gameObject->localScale.y << " " << gameObject->localScale.z << std::endl;
+	stream << "\tTag: " << gameObject->tag << std::endl;
+	stream << "\tLayer: " << (unsigned short)gameObject->layer << std::endl;
+
+	std::vector<unsigned int> componentIDs = Project::componentManager->GetComponentsIDsInGameObject(gameObject);
+	std::vector<Component*> components = Project::componentManager->GetComponentsInGameObject(gameObject);
+
+	if (componentIDs.size() == 0)
+	{
+		stream << "\tComponents: []" << std::endl;
+	}
+	else
+	{
+		stream << "\tComponents: [";
+		for (int i = 0; i < components.size() - 1; i++)
+		{
+			stream << "{" << std::endl << "\t\tClassID: " << componentIDs[i] << std::endl << "\t\tParams: " << std::endl;
+			SaveComponentToScene(componentIDs[i], components[i], stream);
+			stream << "},";
+		}
+
+		size_t lastIndex = components.size() - 1;
+		stream << "{" << std::endl << "\t\tClassID: " << componentIDs[lastIndex] << std::endl << "\t\tParams: " << std::endl;
+		SaveComponentToScene(componentIDs[lastIndex], components[lastIndex], stream);
+		stream << "\t}]" << std::endl;
+	}
+
+	if (gameObject->children.size() == 0)
+	{
+		stream << "\tChildren: []" << std::endl;
+	}
+	else
+	{
+		stream << "\tChildren: [";
+		for (int i = 0; i < gameObject->children.size() - 1; i++)
+		{
+			stream << "{" << std::endl;
+			SaveGameObject(gameObject->children[i], stream);
+			stream << "},";
+		}
+		stream << "{" << std::endl;
+		SaveGameObject(gameObject->children[gameObject->children.size() - 1], stream);
+		stream << "}]" << std::endl;
+	}
+}
 
 bool SceneSaver::SaveScene(uint8_t sceneIndex, const std::string& filename)
 {
@@ -14,7 +67,7 @@ bool SceneSaver::SaveScene(uint8_t sceneIndex, const std::string& filename)
 		Debug::errorLog << "Scene at index " << sceneIndex << " is not loaded" << Debug::endl;
 		return false;
 	}
-	if (!LFileOperations::DoesFileHaveExtension(filename, ".pbscene"))
+	if (!LFileOperations::DoesFileHaveExtension(filename, "pbscene"))
 	{
 		Debug::LogError("Loaded Scene is not a .pbscene file");
 		return false;
@@ -28,8 +81,12 @@ bool SceneSaver::SaveScene(uint8_t sceneIndex, const std::string& filename)
 	}
 
 	sceneFile << "PROJECT BLOO SCENE" << std::endl;
-	sceneFile << "Name: " << "Test Scene" << std::endl; // Fix This
+	sceneFile << "Name: " << LFileOperations::StripFilenameAndExt(filename) << std::endl; // Fix This
 	sceneFile << "Project: " << Project::projectName << std::endl;
+
+	sceneFile << "{" << std::endl;
+	SaveGameObject(&Scene::scenes[sceneIndex].sceneRoot, sceneFile);
+	sceneFile << "}" << std::endl;
 
 	return true;
 }
--- ExperioEditor/Source/Framework/SceneSaver.h
+++ ExperioEditor/Source/Framework/SceneSaver.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <stdint.h>
 #include <string>
+#include <fstream>
+
+class GameObject;
 
 class SceneSaver
 {
+	static void SaveGameObject(GameObject* gameObject, std::ofstream& stream);
 public:
 	static bool SaveScene(uint8_t sceneIndex, const std::string& filename);
 };
