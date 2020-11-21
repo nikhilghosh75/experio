89576d7005a5b60186699f5e38c40ef65da110a7
nikhilghosh75 <nighosh@umich.edu>
2020-11-21 13:00:22 -0500

    Added Random static method to GUID

--- Experio/Source/Runtime/Framework/GUID.h
+++ Experio/Source/Runtime/Framework/GUID.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string>
 #include "../Containers/LString.h"
+#include "../Math/Random.h"
 
 struct GUID
 {
@@ -15,6 +16,18 @@ struct GUID
 	GUID(std::string s)
 	{
 		this->id = (uint64_t)LString::HexStringToLong(s);
+	}
+
+	static GUID Random()
+	{
+		uint64_t subWords[2];
+
+		for (int i = 0; i < 2; i++)
+		{
+			subWords[i] = Random::RandomUIntInRange(0, 0xffffffff);
+		}
+
+		return GUID((uint64_t)subWords[0] << 32 | subWords[1]);
 	}
 
 	std::string ToString()
