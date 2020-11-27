81afd12ec78c7dd21dbb1ffe4b51fb50b4b0a86e
nikhilghosh75 <nighosh@umich.edu>
2020-11-27 15:54:34 -0500

    Implemented Stack Array

--- Experio/Source/Runtime/Containers/TStackArray.h
+++ Experio/Source/Runtime/Containers/TStackArray.h
@@ -4,9 +4,66 @@
 template<typename T, uint32_t N>
 class TStackArray
 {
+	size_t count;
 	T data[N];
 public:
-	TStackArray() {}
+	TStackArray()
+	{
+		count = 0;
+	}
+
+	TStackArray(const T& object, uint32_t num)
+	{
+		if (num > N)
+			num = N;
+		for (uint32_t i = 0; i < num; i++)
+		{
+			data[i] = object;
+		}
+		this->count = num;
+		this->capacity = num;
+	}
+
+	void Append(const T& object)
+	{
+		if (this->count >= N) return;
+		this->data[this->count] = object;
+		this->count++;
+	}
+
+	void Append(const T* objects, uint32_t count)
+	{
+		if (this->count + count >= N) return;
+		for (uint32_t i = 0; i < count; i++)
+		{
+			this->data[this->count + i] = objects[i];
+		}
+		this->count += count;
+	}
+
+	template<typename ... Args>
+	T& EmplaceAppend(Args&& ... args)
+	{
+		if (this->count >= N) return;
+		this->data[this->count] = T(std::forward<Args>(args) ...);
+		this->count++;
+		return this->data[this->count - 2];
+	}
+
+	void Empty()
+	{
+		for (uint32_t i = 0; i < this->count; i++)
+		{
+			data[i].~T();
+		}
+		this->count = 0;
+	}
+
+	void RemoveBack()
+	{
+		this->count--;
+		this->data[this->count].~T();
+	}
 
 	void Fill(const T& element)
 	{
@@ -16,7 +73,8 @@ public:
 		}
 	}
 
-	uint32_t Count() const { return N; }
+	uint32_t Count() const { return count; }
+	uint32_t Capacity() const { return N; }
 
 	T& operator[](uint32_t index) { return data[index]; }
 	const T& operator[](uint32_t index) const { return data[index]; }
