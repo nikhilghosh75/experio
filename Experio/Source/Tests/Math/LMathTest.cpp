79a9a29d45dc875a5e297ae718d2e9bc226a77f2
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2020-12-09 11:21:47 -0500

    Added Vector3 and Math Tests

---
+++ Experio/Source/Tests/Math/LMathTest.cpp
@@ -0,0 +1,74 @@
+#include "../../ExperioEditor/Source/Testing/UnitTestingFramework.h"
+#include "Runtime/Math/LMath.h"
+
+TEST_SUITE("LMath", "Experio/Math");
+
+UNIT_TEST(TestMathAcos, "LMath")
+{
+	ASSERT_ALMOST_EQUAL(LMath::Acos(1), 0);
+	ASSERT_ALMOST_EQUAL(LMath::Acos(0.3f), 1.2661f);
+	ASSERT_ALMOST_EQUAL(LMath::Acos(0.65f), 0.8632f);
+	ASSERT_ALMOST_EQUAL(LMath::Acos(0.97f), 0.2455f);
+	ASSERT_ALMOST_EQUAL(LMath::Acos(0), 1.5707f);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestMathAsin, "LMath")
+{
+	ASSERT_ALMOST_EQUAL(LMath::Asin(0), 1);
+	ASSERT_ALMOST_EQUAL(LMath::Asin(0.45f), 0.4668f);
+	ASSERT_ALMOST_EQUAL(LMath::Asin(0.71f), 0.7895f);
+	ASSERT_ALMOST_EQUAL(LMath::Asin(0.97f), 1.3252f);
+	ASSERT_ALMOST_EQUAL(LMath::Asin(1), 1.5707f);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestMathAbs, "LMath")
+{
+	ASSERT_ALMOST_EQUAL(LMath::Abs(1), 1);
+	ASSERT_ALMOST_EQUAL(LMath::Abs(-1), 1);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestMathAtan, "LMath")
+{
+	ASSERT_ALMOST_EQUAL(LMath::Atan(0), 0);
+	ASSERT_ALMOST_EQUAL(LMath::Atan(0.25f), 0.2450f);
+	ASSERT_ALMOST_EQUAL(LMath::Asin(0.52f), 0.4795f);
+	ASSERT_ALMOST_EQUAL(LMath::Asin(1.11f), 0.8375f);
+
+	ASSERT_ALMOST_EQUAL(LMath::Atan2(0, 1), HALFPI);
+	ASSERT_ALMOST_EQUAL(LMath::Atan2(1, 0), -HALFPI);
+	ASSERT_ALMOST_EQUAL(LMath::Atan2(1, 1), 0.7854f);
+	ASSERT_ALMOST_EQUAL(LMath::Atan2(-2, 1), 2.6780f);
+	ASSERT_ALMOST_EQUAL(LMath::Atan2(7, -3), -0.4049f);
+	ASSERT_ALMOST_EQUAL(LMath::Atan2(-8, -3), -2.7828f);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestMathBetween, "LMath")
+{
+	ASSERT_TRUE(LMath::Between(0.f, 1.f, 0.5f), "");
+	ASSERT_FALSE(LMath::Between(-1.f, 2.f, 3.f), "");
+
+	TEST_END();
+}
+
+UNIT_TEST(TestMathBinomial, "LMath")
+{
+	ASSERT_ALMOST_EQUAL(LMath::BinomialCoefficent(10, 3), 120);
+	ASSERT_ALMOST_EQUAL(LMath::BinomialCoefficent(10, 7), 120);
+	ASSERT_ALMOST_EQUAL(LMath::BinomialCoefficent(10, 0), 1);
+	ASSERT_ALMOST_EQUAL(LMath::BinomialCoefficent(13, 13), 1);
+
+	TEST_END();
+}
+
+FUnitTestingOutcome RunLMathTests()
+{
+	return suite.RunTests();
+}
---
+++ Experio/Source/Tests/Math/Vector3Test.cpp
@@ -0,0 +1,164 @@
+#include "../../ExperioEditor/Source/Testing/UnitTestingFramework.h"
+#include "Runtime/Math/FVector3.h"
+
+TEST_SUITE("Vector3", "Experio/Math");
+
+UNIT_TEST(TestVector3Magnitude, "Vector3")
+{
+	FVector3 v(3, 3, 0);
+
+	ASSERT_ALMOST_EQUAL(FVector3::Magnitude(v), 4.243f);
+	ASSERT_ALMOST_EQUAL(FVector3::SqrMagnitude(v), 18);
+
+	FVector3 zero(0, 0, 0);
+	ASSERT_ALMOST_EQUAL(FVector3::Magnitude(v), 0);
+	ASSERT_ALMOST_EQUAL(FVector3::SqrMagnitude(v), 0);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestVector3Normalized, "Vector3")
+{
+	FVector3 v(3, 6, 9);
+	FVector3 normalized = FVector3::Normalized(v);
+
+	ASSERT_FALSE(FVector3::IsNormalized(v), "");
+	ASSERT_TRUE(FVector3::IsNormalized(normalized), "");
+
+	ASSERT_ALMOST_EQUAL(FVector3::Magnitude(normalized), 1);
+	ASSERT_ALMOST_EQUAL(FVector3::SqrMagnitude(normalized), 1);
+
+	ASSERT_ALMOST_EQUAL(normalized.x, 0.26726f);
+	ASSERT_ALMOST_EQUAL(normalized.y, 0.53452f);
+	ASSERT_ALMOST_EQUAL(normalized.z, 0.80178f);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestVector3Zero, "Vector3")
+{
+	FVector3 v(3, 6, 9);
+	FVector3 zero(0, 0, 0);
+	FVector3 up(0, 1, 0);
+	FVector3 right(1, 0, 0);
+	FVector3 in(0, 0, 1);
+
+	ASSERT_FALSE(FVector3::IsZero(v), "");
+	ASSERT_TRUE(FVector3::IsZero(zero), "");
+	ASSERT_FALSE(FVector3::IsZero(up), "");
+	ASSERT_FALSE(FVector3::IsZero(right), "");
+	ASSERT_FALSE(FVector3::IsZero(in), "");
+
+	TEST_END();
+}
+
+UNIT_TEST(TestVector3Products, "Vector3")
+{
+	FVector3 v1(1, 2, 3);
+	FVector3 v2(4, 5, 6);
+
+	float dot = FVector3::Dot(v1, v2);
+	FVector3 cross = FVector3::Cross(v1, v2);
+
+	ASSERT_ALMOST_EQUAL(dot, 32);
+	ASSERT_ALMOST_EQUAL(cross.x, -3);
+	ASSERT_ALMOST_EQUAL(cross.y, 6);
+	ASSERT_ALMOST_EQUAL(cross.z, -3);
+
+	dot = FVector3::Dot(v2, v1);
+	cross = FVector3::Cross(v2, v1);
+	ASSERT_ALMOST_EQUAL(dot, 32);
+	ASSERT_ALMOST_EQUAL(cross.x, -3);
+	ASSERT_ALMOST_EQUAL(cross.y, 6);
+	ASSERT_ALMOST_EQUAL(cross.z, -3);
+
+	ASSERT_ALMOST_EQUAL(FVector3::Dot(v1, v1), 6);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestVector3Lerp, "Vector3")
+{
+	FVector3 v1(1, 1, 1);
+	FVector3 v2(9, 13, 17);
+
+	FVector3 zero = FVector3::Lerp(v1, v2, 0);
+	ASSERT_ALMOST_EQUAL(zero.x, 1);
+	ASSERT_ALMOST_EQUAL(zero.y, 1);
+	ASSERT_ALMOST_EQUAL(zero.z, 1);
+
+	FVector3 twentyFive = FVector3::Lerp(v1, v2, 0.25f);
+	ASSERT_ALMOST_EQUAL(twentyFive.x, 3);
+	ASSERT_ALMOST_EQUAL(twentyFive.y, 4);
+	ASSERT_ALMOST_EQUAL(twentyFive.z, 5);
+
+	FVector3 half = FVector3::Lerp(v1, v2, 0.5f);
+	ASSERT_ALMOST_EQUAL(half.x, 5);
+	ASSERT_ALMOST_EQUAL(half.y, 7);
+	ASSERT_ALMOST_EQUAL(half.z, 9);
+
+	FVector3 threeQuarter = FVector3::Lerp(v1, v2, 0.75f);
+	ASSERT_ALMOST_EQUAL(threeQuarter.x, 7);
+	ASSERT_ALMOST_EQUAL(threeQuarter.y, 10);
+	ASSERT_ALMOST_EQUAL(threeQuarter.z, 13);
+
+	FVector3 one = FVector3::Lerp(v1, v2, 1);
+	ASSERT_ALMOST_EQUAL(one.x, 9);
+	ASSERT_ALMOST_EQUAL(one.y, 13);
+	ASSERT_ALMOST_EQUAL(one.z, 17);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestVector3Operations, "Vector3")
+{
+	FVector3 v1(2, 3, 4);
+	FVector3 v2(6, 7, 8);
+
+	FVector3 addResult = v1 + v2;
+	ASSERT_ALMOST_EQUAL(addResult.x, 8);
+	ASSERT_ALMOST_EQUAL(addResult.y, 10);
+	ASSERT_ALMOST_EQUAL(addResult.z, 12);
+
+	addResult += FVector3(1, 1, 1);
+	ASSERT_ALMOST_EQUAL(addResult.x, 9);
+	ASSERT_ALMOST_EQUAL(addResult.y, 11);
+	ASSERT_ALMOST_EQUAL(addResult.z, 13);
+
+	FVector3 subtractResult = v2 - v1;
+	ASSERT_ALMOST_EQUAL(subtractResult.x, 4);
+	ASSERT_ALMOST_EQUAL(subtractResult.y, 4);
+	ASSERT_ALMOST_EQUAL(subtractResult.z, 4);
+
+	subtractResult -= FVector3(2, 3, 4);
+	ASSERT_ALMOST_EQUAL(subtractResult.x, 2);
+	ASSERT_ALMOST_EQUAL(subtractResult.y, 1);
+	ASSERT_ALMOST_EQUAL(subtractResult.z, 0);
+
+	FVector3 multiplyResult = v1 * 4.f;
+	ASSERT_ALMOST_EQUAL(multiplyResult.x, 8);
+	ASSERT_ALMOST_EQUAL(multiplyResult.y, 12);
+	ASSERT_ALMOST_EQUAL(multiplyResult.z, 16);
+
+	multiplyResult *= 0.5f;
+	ASSERT_ALMOST_EQUAL(multiplyResult.x, 4);
+	ASSERT_ALMOST_EQUAL(multiplyResult.y, 4);
+	ASSERT_ALMOST_EQUAL(multiplyResult.z, 4);
+
+	FVector3 divideResult = v2 / 2;
+	ASSERT_ALMOST_EQUAL(divideResult.x, 3.f);
+	ASSERT_ALMOST_EQUAL(divideResult.y, 3.5f);
+	ASSERT_ALMOST_EQUAL(divideResult.z, 4.f);
+
+	divideResult /= 2;
+	ASSERT_ALMOST_EQUAL(divideResult.x, 1.5f);
+	ASSERT_ALMOST_EQUAL(divideResult.y, 1.75f);
+	ASSERT_ALMOST_EQUAL(divideResult.z, 2.f);
+
+	TEST_END();
+}
+
+FUnitTestingOutcome RunVector3Tests()
+{
+	return suite.RunTests();
+}
