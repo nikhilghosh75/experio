13fea67363e72ffe3095115bef8d62914d99e04e
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2020-12-12 17:24:41 -0500

    Implemented functions in LStatistics

--- Experio/Source/Runtime/Data/LStatistics.cpp
+++ Experio/Source/Runtime/Data/LStatistics.cpp
@@ -10,6 +10,36 @@ float LStatistics::Average(const std::vector<float> data)
 	return Sum(data.data(), data.size()) / data.size();
 }
 
+float LStatistics::Max(float * data, size_t count)
+{
+	float max = data[0];
+	for (size_t i = 0; i < count; i++)
+	{
+		if (data[i] > max) { max = data[i]; }
+	}
+	return max;
+}
+
+float LStatistics::Max(std::vector<float> data)
+{
+	return Max(data.data(), data.size());
+}
+
+float LStatistics::Min(float * data, size_t count)
+{
+	float min = data[0];
+	for (size_t i = 0; i < count; i++)
+	{
+		if (data[i] < min) { min = data[i]; }
+	}
+	return min;
+}
+
+float LStatistics::Min(std::vector<float> data)
+{
+	return Min(data.data(), data.size());
+}
+
 float LStatistics::Sum(const float * data, size_t count)
 {
 	float sum = 0.0f;
