6e5086d46979b14ef3ac458306f40c00a0615f32
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2020-12-15 12:19:23 -0500

    Fixed TLinearAllocator

--- Experio/Source/Runtime/Memory/TLinearAllocator.cpp
+++ Experio/Source/Runtime/Memory/TLinearAllocator.cpp
@@ -5,6 +5,7 @@
 
 void TestLinearAllocator()
 {
+	/*
 	TLinearAllocator<int>* allocator = new TLinearAllocator<int>((size_t)400);
 	allocator->Init();
 	int* testArray;
@@ -13,94 +14,5 @@ void TestLinearAllocator()
 	testArray[1] = 3;
 
 	std::cout << testArray[1] << std::endl;
-}
-
-template<class T>
-TLinearAllocator<T>::TLinearAllocator()
-{
-	this->memoryUsed = 0;
-	this->allocationsMade = 0;
-}
-
-template<class T>
-TLinearAllocator<T>::TLinearAllocator(size_t size) 
-{
-	this->totalSize = size;
-    this->memoryUsed = 0;
-    this->allocationsMade = 0;
-
-	this->start = nullptr;
-}
-
-template<class T>
-TLinearAllocator<T>::TLinearAllocator(size_t size, void* start)
-{
-	this->start = start;
-    this->totalSize = size;
-    this->memoryUsed = 0;
-    this->allocationsMade = 0;
-
-	if (size <= 0)
-	{
-		// TO-DO: Add Error
-	}
-
-	currentPosition = start;
-}
-
-template<class T>
-TLinearAllocator<T>::~TLinearAllocator()
-{
-	currentPosition = nullptr;
-}
-
-template<class T>
-void * TLinearAllocator<T>::Allocate(size_t size, uint8_t alignment)
-{
-	size_t padding = 0;
-    size_t paddedAddress = 0;
-	size_t currentAddress = (size_t)this->start + this->offset;
-
-    if (alignment != 0 && this->memoryUsed % alignment != 0) 
-	{
-        // Alignment is required. Find the next aligned memory address and update offset
-        padding = LPointerOperations::CalculatePadding(currentAddress, alignment);
-    }
-
-    if (this->offset + padding + size > this->totalSize) 
-	{
-        return nullptr;
-    }
-
-    this->offset += padding;
-    size_t nextAddress = currentAddress + padding;
-	this->offset += size;
-	this->allocationsMade++;
-	this->memoryUsed = offset;
-
-    return (void*) nextAddress;
-}
-
-template<class T>
-void TLinearAllocator<T>::Deallocate(void * p)
-{
-	throw "Deallocate() invalid with Linear Allocator. Use clear() instead";
-}
-
-template<class T>
-void TLinearAllocator<T>::Init()
-{
-	if(this->start != nullptr)
-	{
-		free(this->start);
-	}
-	this->start = malloc(this->totalSize);
-}
-
-template<class T>
-void TLinearAllocator<T>::Clear()
-{
-	this->allocationsMade = 0;
-	this->memoryUsed = 0;
-	currentPosition = this->start;
+	*/
 }
--- Experio/Source/Runtime/Memory/TLinearAllocator.h
+++ Experio/Source/Runtime/Memory/TLinearAllocator.h
@@ -1,19 +1,93 @@
 #pragma once
 #include "TAllocator.h"
+#include "LPointerOperations.h"
+#include <cstddef>
 
 template <class T> 
 class TLinearAllocator : public TAllocator<T>
 {
 public:
-    TLinearAllocator();
-    TLinearAllocator(size_t size);
-    TLinearAllocator(size_t size, void* start);
-    ~TLinearAllocator();
+	TLinearAllocator()
+	{
+		this->memoryUsed = 0;
+		this->allocationsMade = 0;
+		this->start = nullptr;
+	}
 
-    void* Allocate(size_t size, uint8_t alignment) override;
-    void Deallocate(void* p) override;
-    void Init() override;
-    void Clear();
+	TLinearAllocator(size_t size)
+	{
+		this->totalSize = size;
+		this->memoryUsed = 0;
+		this->allocationsMade = 0;
+
+		this->start = malloc(size); // Change Later
+	}
+
+	TLinearAllocator(void* start, size_t size)
+	{
+		this->start = start;
+		this->totalSize = size;
+		this->memoryUsed = 0;
+		this->allocationsMade = 0;
+	}
+
+	~TLinearAllocator()
+	{
+		if (this->start != nullptr)
+		{
+			delete this->start;
+		}
+	}
+
+	void* Allocate(size_t size, uint8_t alignment) override
+	{
+		size_t padding = 0;
+		size_t paddedAddress = 0;
+		size_t currentAddress = (size_t)this->start + this->offset;
+
+		if (alignment != 0 && this->memoryUsed % alignment != 0)
+		{
+			// Alignment is required. Find the next aligned memory address and update offset
+			padding = LPointerOperations::CalculatePadding(currentAddress, alignment);
+		}
+
+		if (this->offset + padding + size > this->totalSize)
+		{
+			return nullptr;
+		}
+
+		this->offset += padding;
+		size_t nextAddress = currentAddress + padding;
+		this->offset += size;
+		this->allocationsMade++;
+		this->memoryUsed = offset;
+
+		return (void*)nextAddress;
+	}
+
+	void Deallocate(void* p) override
+	{
+		// TO-DO: Create actual exceptions
+		throw "Deallocate() invalid with Linear Allocator. Use clear() instead";
+	}
+
+	void Init() override
+	{
+		if (this->start == nullptr)
+		{
+			this->start = malloc(this->totalSize);
+		}
+
+		this->offset = 0;
+	}
+
+	void Clear()
+	{
+		this->allocationsMade = 0;
+		this->memoryUsed = 0;
+		this->currentPosition = this->start;
+		this->offset = 0;
+	}
 
 private:
     void* currentPosition;
