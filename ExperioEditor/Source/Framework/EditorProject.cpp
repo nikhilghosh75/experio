2d55534ad2aec9f2cdc4e154cd247fe93b6bd317
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2020-12-22 12:28:15 -0500

    Added classes to temp setup

--- ExperioEditor/Source/Framework/EditorProject.cpp
+++ ExperioEditor/Source/Framework/EditorProject.cpp
@@ -9,7 +9,7 @@
 
 THashtable<unsigned int, FComponentInfo> EditorProject::componentClasses;
 FVersion EditorProject::experioVersion;
-// CodeProject EditorProject::gameProject;
+CodeProject EditorProject::gameProject(ECodingLanguage::CPlusPlus);
 
 std::string EditorProject::projectName;
 std::string EditorProject::username;
@@ -89,4 +89,47 @@ void EditorProject::TempSetup()
 	EditorProject::componentClasses.Insert(102, FComponentInfo("ParticleSystem", true));
 	EditorProject::componentClasses.Insert(103, FComponentInfo("Billboard", true));
 	EditorProject::componentClasses.Insert(104, FComponentInfo("TextComponent", true));
+
+	gameProject.filepath = EditorApplication::sourceFilePath;
+	gameProject.Generate();
+
+	CodeClass virtualCamera("VirtualCamera");
+	virtualCamera.inheritance.push_back("Component");
+	virtualCamera.params.emplace_back("float", "priority", ECodeAccessType::Public);
+	virtualCamera.params.emplace_back("float", "fieldOfView", ECodeAccessType::Public);
+	virtualCamera.params.emplace_back("float", "priority", ECodeAccessType::Public);
+	virtualCamera.params.emplace_back("float", "fieldOfView", ECodeAccessType::Public);
+
+	CodeClass meshComponent("MeshComponent");
+	meshComponent.inheritance.push_back("Component");
+	meshComponent.params.emplace_back("MeshMaterial*", "material", ECodeAccessType::Public);
+	meshComponent.params.emplace_back("MeshRef", "meshData", ECodeAccessType::Public);
+	meshComponent.params.emplace_back("bool", "isVisible", ECodeAccessType::Public);
+
+	CodeClass particleSystem("ParticleSystem");
+	particleSystem.inheritance.push_back("Component");
+
+	CodeClass billboard("Billboard");
+	billboard.inheritance.push_back("Component");
+	billboard.params.emplace_back("TextureRef", "billboardTexture", ECodeAccessType::Public);
+	billboard.params.emplace_back("EBillboardSizeType", "sizeType", ECodeAccessType::Public);
+	billboard.params.emplace_back("EBilboardOrientation", "orientation", ECodeAccessType::Public);
+	billboard.params.emplace_back("FVector2", "billboardSize", ECodeAccessType::Public);
+
+	CodeClass textComponent("TextComponent");
+	textComponent.inheritance.push_back("Component");
+	textComponent.params.emplace_back("float", "margins", ECodeAccessType::Public);
+	textComponent.params.emplace_back("int", "fontSize", ECodeAccessType::Public);
+	textComponent.params.emplace_back("std::string", "text", ECodeAccessType::Public);
+	textComponent.params.emplace_back("FontRef", "font", ECodeAccessType::Public);
+	textComponent.params.emplace_back("Shader*", "shader", ECodeAccessType::Public);
+
+	gameProject.PushClass(virtualCamera);
+	gameProject.PushClass(meshComponent);
+	gameProject.PushClass(particleSystem);
+	gameProject.PushClass(billboard);
+	gameProject.PushClass(textComponent);
+
+	gameProject.EmplaceEnum("EBillboardSizeType", EEnumDataType::UBYTE);
+	gameProject.EmplaceEnum("EBilboardOrientation", EEnumDataType::UBYTE);
 }
