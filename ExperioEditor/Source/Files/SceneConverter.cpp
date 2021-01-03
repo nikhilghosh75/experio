fd167db9859a15fa92013ce963b4f87417bd332b
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2021-01-03 15:56:01 -0500

    Started work on experimental scene converter

--- ExperioEditor/Source/Files/SceneConverter.cpp
+++ ExperioEditor/Source/Files/SceneConverter.cpp
@@ -6,20 +6,16 @@
 #include "Runtime\Framework\Project.h"
 #include "Runtime\Framework\Scene.h"
 #include "Runtime\Framework\SceneLoader.h"
-#include <fstream>
 
-const uint32_t sizeOfBaseGameObject = 56;
+const uint32_t sizeOfGameObject = 88;
+
+#define PB_HEADER_LENGTH 68
 
 extern void SaveBinaryComponentToScene(unsigned int componentID, Component* component, std::ofstream& stream);
 
 extern size_t SerializedSizeOfComponent(unsigned int classId);
 
 extern std::vector<std::string> GetParamsList(unsigned int classId);
-
-uint32_t SizeOfGameObject(const GameObject* object)
-{
-	return sizeOfBaseGameObject + object->name.size();
-}
 
 void SceneConverter::ConvertSceneToBinary(const std::string & fromFilepath, const std::string & toFilepath)
 {
@@ -35,12 +31,7 @@ void SceneConverter::ConvertSceneToBinary(const std::string & fromFilepath, cons
 		return;
 	}
 
-	// Load From Filepath
-	if (!Scene::IsSceneAtFilepathLoaded(fromFilepath))
-	{
-		SceneLoader::LoadSceneFromFile(fromFilepath, MAX_SCENES - 3);
-	}
-
+	SceneLoader::LoadSceneFromFile(fromFilepath, MAX_SCENES - 3);
 	Scene& currentScene = Scene::scenes[MAX_SCENES - 3];
 
 	std::ofstream outFile(toFilepath, std::ios::out | std::ios::binary);
@@ -50,13 +41,13 @@ void SceneConverter::ConvertSceneToBinary(const std::string & fromFilepath, cons
 
 	uint32_t sizeOfGameObjects = 0;
 	ForEach(&currentScene.sceneRoot, [&sizeOfGameObjects](GameObject* object) {
-		sizeOfGameObjects += sizeOfBaseGameObject + object->name.size();
+		sizeOfGameObjects += sizeOfGameObject + object->name.size();
 	});
 
 	uint32_t gameObjectOffset = 68;
 	uint32_t componentsOffset = gameObjectOffset + sizeOfGameObjects;
 	uint32_t sceneDataAssetID = AssetMapSaver::NextAssetMapIndex();
-	uint32_t numComponents = Project::componentManager->ComponentCount();
+	uint32_t numComponents = currentScene.GetNumComponents();
 	uint32_t numGameObjects = currentScene.GetNumGameObjects();
 
 	outFile.write((char*)&gameObjectOffset, 4);
@@ -130,4 +121,91 @@ void SceneConverter::ConvertSceneToBinary(const std::string & fromFilepath, cons
 
 	outFile.close();
 	Scene::UnloadScene(MAX_SCENES - 3);
+}
+
+void SceneConverter::ExperimentalConvertSceneToBinary(const std::string & fromFilepath, const std::string & toFilepath)
+{
+	if (!LFileOperations::DoesFileHaveExtension(fromFilepath, ".pbscene"))
+	{
+		Debug::LogError("From Filepath is not a correct filepath");
+		return;
+	}
+
+	if (!LFileOperations::DoesFileHaveExtension(toFilepath, ".pbbscene"))
+	{
+		Debug::LogError("To Filepath is not a correct filepath");
+		return;
+	}
+
+	std::ifstream inFile(fromFilepath);
+	FileBuffer buffer = LFileOperations::ReadTrimmedFileToBuffer(inFile);
+
+	std::ofstream outFile(toFilepath, std::ios::out | std::ios::binary);
+
+	GenerateHeader(buffer, outFile);
+
+
+}
+
+void SceneConverter::GenerateHeader(const FileBuffer & buffer, std::ofstream & outFile)
+{
+	outFile.write("PBBSCENE", 8);
+	outFile.write("0000", 4); // Bytes 8-11
+
+	uint32_t numGameObjects = GetNumGameObjects(buffer);
+	uint32_t sizeOfGameObjects = numGameObjects * sizeOfGameObject;
+	uint32_t numComponents = GetNumComponents(buffer);
+	uint32_t gameObjectOffset = PB_HEADER_LENGTH;
+	uint32_t componentsOffset = PB_HEADER_LENGTH + sizeOfGameObjects;
+	uint32_t sceneDataAssetID = AssetMapSaver::NextAssetMapIndex();
+
+	outFile.write((char*)&gameObjectOffset, 4);
+	outFile.write((char*)&componentsOffset, 4);
+	outFile.write((char*)&sceneDataAssetID, 4);
+	outFile.write("0000", 4);
+	outFile.write((char*)&numComponents, 4);
+	outFile.write((char*)&numGameObjects, 4);
+
+	std::string name = GetName(buffer);
+	outFile.write(name.c_str(), 32);
+}
+
+uint32_t SceneConverter::GetNumGameObjects(const FileBuffer & buffer)
+{
+	uint32_t numGameObjects = 0;
+	for (size_t i = 0; i < buffer.Size(); i++)
+	{
+		if (buffer.CompareSubstr("Transform: ", i))
+		{
+			numGameObjects++;
+			i += 20; // Figure out good number later
+		}
+	}
+	return numGameObjects;
+}
+
+uint32_t SceneConverter::GetNumComponents(const FileBuffer & buffer)
+{
+	uint32_t numComponents = 0;
+	for (size_t i = 0; i < buffer.Size(); i++)
+	{
+		if (buffer.CompareSubstr("ClassID:", i))
+		{
+			numComponents++;
+			i += 10; // Figure out good number later
+		}
+	}
+	return numComponents;
+}
+
+std::string SceneConverter::GetName(const FileBuffer & buffer)
+{
+	size_t indexOfName = buffer.Find("Name: ");
+	size_t indexOfNewLine = buffer.Find('\n', indexOfName);
+	return buffer.Substr(indexOfName, indexOfNewLine);
+}
+
+void SceneConverter::GenerateGameObject(const FileBuffer & buffer, std::ofstream & outFile)
+{
+	
 }
--- ExperioEditor/Source/Files/SceneConverter.h
+++ ExperioEditor/Source/Files/SceneConverter.h
@@ -1,8 +1,24 @@
 #pragma once
 #include <string>
+#include <fstream>
+
+class FileBuffer;
 
 class SceneConverter
 {
 public:
 	static void ConvertSceneToBinary(const std::string& fromFilepath, const std::string& toFilepath);
+
+	static void ExperimentalConvertSceneToBinary(const std::string& fromFilepath, const std::string& toFilepath);
+
+private:
+	static void GenerateHeader(const FileBuffer& buffer, std::ofstream& outFile);
+
+	static uint32_t GetNumGameObjects(const FileBuffer& buffer);
+
+	static uint32_t GetNumComponents(const FileBuffer& buffer);
+
+	static std::string GetName(const FileBuffer& buffer);
+
+	static void GenerateGameObject(const FileBuffer& buffer, std::ofstream& outFile);
 };
