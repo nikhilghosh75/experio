fc0551d09184ea5548f2d2cd0fc043f6edebfbd9
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2021-01-10 12:53:20 -0500

    Added context menus to default components

--- ExperioEditor/Source/ComponentEditor/Renderable/BillboardComponentEditor.cpp
+++ ExperioEditor/Source/ComponentEditor/Renderable/BillboardComponentEditor.cpp
@@ -1,4 +1,6 @@
 #include "BillboardComponentEditor.h"
+#include "../../AssetViewers/ImageViewer.h"
+#include "../../Core/EditorApplication.h"
 #include "Runtime/Rendering/ImGui/LImGui.h"
 
 void BillboardEditor::Display(Component * billboardComponent)
@@ -9,4 +11,15 @@ void BillboardEditor::Display(Component * billboardComponent)
 	LImGui::DisplayEnum<EBillboardSizeType>(billboard->sizeType, "Size Type");
 	LImGui::DisplayEnum<EBilboardOrientation>(billboard->orientation, "Orientation");
 	LImGui::DisplayVector2(billboard->billboardSize, "Size");
+}
+
+void BillboardEditor::ContextMenu(Component * billboardComponent)
+{
+	Billboard* billboard = (Billboard*)billboardComponent;
+
+	if (ImGui::Selectable("View Image"))
+	{
+		ImageViewer* viewer = (ImageViewer*)EditorApplication::AddModule(new ImageViewer());
+		viewer->loadedRef = billboard->billboardTexture;
+	}
 }
