833f9c0592275064eba12ab06a093a4bd0e1f31c
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2021-01-20 13:10:40 -0500

    Fixed Import Scripting

--- ExperioEditor/Source/Framework/ImportSystem.cpp
+++ ExperioEditor/Source/Framework/ImportSystem.cpp
@@ -3,6 +3,8 @@
 #include "Runtime/Files/LFileOperations.h"
 namespace fs = std::filesystem;
 
+ImportScriptManager* ImportSystem::manager;
+
 void ImportSystem::Import(const std::string & fromFilepath, const std::string & toDirectory)
 {
 	EAssetType type = LFileOperations::GetFileType(fromFilepath);
@@ -13,9 +15,32 @@ void ImportSystem::Import(const std::string & fromFilepath, const std::string & 
 
 	std::string stripFilename = LFileOperations::StripFilename(fromFilepath);
 	fs::copy(fromFilepath, toDirectory + "/" + stripFilename);
+	
+	FImportInfo info;
+	info.filepath = toDirectory + "/" + stripFilename;
+
+	for (size_t i = 0; i < manager->importFunctions.size(); i++)
+	{
+		EImportHandlerResult result = manager->importFunctions[i](info);
+		if (result != EImportHandlerResult::NotHandled) break;
+	}
 }
 
 bool ImportSystem::RejectAssetType(EAssetType type)
 {
 	return type == EAssetType::Meta || type == EAssetType::NonEngineCode;
+}
+
+FImportInfo::FImportInfo()
+{
+}
+
+EAssetType FImportInfo::AssetType()
+{
+	return EAssetType();
+}
+
+FFileCategory FImportInfo::FileCategory()
+{
+	return FFileCategory();
 }
--- ExperioEditor/Source/Framework/ImportSystem.h
+++ ExperioEditor/Source/Framework/ImportSystem.h
@@ -1,12 +1,54 @@
 #pragma once
+#include <functional>
 #include <string>
 #include "Runtime/Files/FFileCategory.h"
+
+struct FImportInfo
+{
+	std::string filepath;
+	
+	FImportInfo();
+
+	EAssetType AssetType();
+
+	FFileCategory FileCategory();
+};
+
+enum class EImportHandlerResult
+{
+	Handled,
+	NotHandled,
+	Failed
+};
+
+class ImportScriptManager
+{
+public:
+	std::vector<std::function<EImportHandlerResult(FImportInfo)>> importFunctions;
+};
 
 class ImportSystem
 {
 public:
+	static ImportScriptManager* manager;
+
 	static void Import(const std::string& fromFilepath, const std::string& toDirectory);
 
 private:
 	static bool RejectAssetType(EAssetType type);
 };
+
+class ImportRegisterer
+{
+public:
+	ImportRegisterer(std::function<EImportHandlerResult(FImportInfo)> func)
+	{
+		if (ImportSystem::manager == nullptr)
+		{
+			ImportSystem::manager = new ImportScriptManager();
+		}
+		ImportSystem::manager->importFunctions.push_back(func);
+	}
+};
+
+#define IMPORT_FUNCTION(_funcName_) ImportRegisterer _funcName_register(##_funcName_);
