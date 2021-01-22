895964ae0e623e9a13fe9e59c8016d4239ac3db0
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2021-01-22 10:34:27 -0500

    Added CalculateModelMatrix to MeshComponent (will likely move later)

--- Experio/Source/Runtime/Rendering/Components/MeshComponent.cpp
+++ Experio/Source/Runtime/Rendering/Components/MeshComponent.cpp
@@ -53,3 +53,17 @@ void MeshComponent::RecalculateModelMatrix()
 
 	modelMatrix = translationMatrix * rotationMatrix * scaleMatrix;
 }
+
+glm::mat4 MeshComponent::CalculateModelMatrix(FVector3 position, FQuaternion rotation, FVector3 scale)
+{
+	glm::mat4 translationMatrix = glm::translate((glm::vec3)position);
+	glm::mat4 rotationMatrix = glm::toMat4((glm::quat)rotation);
+	glm::mat4 scaleMatrix = glm::scale((glm::vec3)scale);
+
+	return translationMatrix * rotationMatrix * scaleMatrix;
+}
+
+glm::mat4 MeshComponent::CalculateModelMatrix(const GameObject & gameObject)
+{
+	return CalculateModelMatrix(gameObject.GetPosition(), gameObject.GetRotation(), gameObject.GetScale());
+}
