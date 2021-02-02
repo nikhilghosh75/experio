2df48d8aa0cb7859f5b82dc4fccc02673edb00ad
nikhilghosh75 <31861591+nikhilghosh75@users.noreply.github.com>
2021-02-02 10:44:27 -0500

    Fixed ColorPalette

--- Experio/Source/Runtime/Math/ColorPalette.h
+++ Experio/Source/Runtime/Math/ColorPalette.h
@@ -2,35 +2,38 @@
 
 #include "FColor.h"
 
-namespace ColorPalette 
+struct FColor;
+
+class ColorPalette 
 {
-	FColor White(1.f, 1.f, 1.f);
-	FColor Black(0.f, 0.f, 0.f);
-	FColor Night(12, 9, 10);
-	FColor Gunmetal(44, 27, 23);
-	FColor Midnight(43, 27, 23);
-	FColor Charcoal(52, 40, 44);
-	FColor DarkSlateGrey(37, 56, 60);
-	FColor Oil(59, 49, 49);
-	FColor BlackCat(65, 56, 57);
-	FColor Iridium(61, 60, 58);
-	FColor BlackEel(70, 62, 63);
-	FColor BlackCow(77, 70, 70);
-	FColor GrayWolf(80, 74, 75);
-	FColor VampireGray(86, 88, 88);
-	FColor GrayDolphin(92, 88, 88);
-	FColor CarbonGray(98, 93, 93);
-	FColor AshGray(102, 99, 98);
-	FColor CloudyGray(109, 105, 104);
-	FColor SmokeyGray(114, 110, 109);
-	FColor Gray(115, 111, 110);
-	FColor Granite(131, 126, 124);
-	FColor BattleshipGray(132, 132, 130);
-	FColor GrayCloud(182, 182, 180);
-	FColor GrayGoose(209, 208, 206);
-	FColor Platinum(229, 228, 226);
-	FColor MetallicSilver(188, 198, 204);
-	FColor BlueGray(152, 175, 199);
-	FColor LightSlateGray(109, 123, 141);
-	FColor Red(1.f, 0.f, 0.f);
-}
+public:
+	static const FColor const White;
+	static const FColor const Black;
+	static const FColor const Night;
+	static const FColor const Gunmetal;
+	static const FColor const Midnight;
+	static const FColor const Charcoal;
+	static const FColor const DarkSlateGrey;
+	static const FColor const Oil;
+	static const FColor const BlackCat;
+	static const FColor const Iridium;
+	static const FColor const BlackEel;
+	static const FColor const BlackCow;
+	static const FColor const GrayWolf;
+	static const FColor const VampireGray;
+	static const FColor const GrayDolphin;
+	static const FColor const CarbonGray;
+	static const FColor const AshGray;
+	static const FColor const CloudyGray;
+	static const FColor const SmokeyGray;
+	static const FColor const Gray;
+	static const FColor const Granite;
+	static const FColor const BattleshipGray;
+	static const FColor const GrayCloud;
+	static const FColor const GrayGoose;
+	static const FColor const Platinum;
+	static const FColor const MetallicSilver;
+	static const FColor const BlueGray;
+	static const FColor const LightSlateGray;
+	static const FColor const Red;
+};
