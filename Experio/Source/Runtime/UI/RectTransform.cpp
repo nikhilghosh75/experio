24bcb94df6607b21f7ec4348eb12bf60cca4326b
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-03-15 12:51:30 -0400

    Created basic functionality for RectTransform

--- Experio/Source/Runtime/UI/RectTransform.cpp
+++ Experio/Source/Runtime/UI/RectTransform.cpp
@@ -2,9 +2,70 @@
 
 RectTransform::RectTransform()
 {
+	this->rect = FRect();
+	this->pivot = FVector2(0.5f, 0.5f);
+	this->rotation = 0;
+	this->anchorType = EAnchorType::Center;
 }
 
 RectTransform::RectTransform(FVector2 min, FVector2 max)
 {
 	this->rect = FRect(min, max);
+	this->pivot = FVector2(0.5f, 0.5f);
+	this->rotation = 0;
+	this->anchorType = EAnchorType::Center;
 }
+
+RectTransform::RectTransform(FVector2 min, FVector2 max, FVector2 pivot)
+{
+	this->rect = FRect(min, max);
+	this->pivot = pivot;
+	this->rotation = 0;
+	this->anchorType = EAnchorType::Center;
+}
+
+RectTransform::RectTransform(FVector2 min, FVector2 max, FVector2 pivot, float rotation)
+{
+	this->rect = FRect(min, max);
+	this->pivot = pivot;
+	this->rotation = rotation;
+	this->anchorType = EAnchorType::Center;
+}
+
+bool RectTransform::IsFillAnchorType(EAnchorType anchorType)
+{
+	if (anchorType == EAnchorType::Bottom || anchorType == EAnchorType::Center
+		|| anchorType == EAnchorType::Full || anchorType == EAnchorType::Left
+		|| anchorType == EAnchorType::Middle || anchorType == EAnchorType::Right
+		|| anchorType == EAnchorType::Upper)
+	{
+		return true;
+	}
+	return false;
+}
+
+FRect RectTransform::MergeRectTransform(FRect rect, FRect childToParent, EAnchorType childAnchorType, FVector2 screenDimensions)
+{
+	switch (childAnchorType)
+	{
+	case EAnchorType::Full: return MergeRectFull(rect, childToParent, screenDimensions);
+	}
+}
+
+FRect RectTransform::MergeRectFull(FRect rect, FRect childToParent, FVector2 screenDimensions)
+{
+	FRect newRect;
+
+	if (rect.min.x < 0)
+	{
+		newRect.min = rect.min + childToParent.min;
+		newRect.max = rect.max + childToParent.max;
+	}
+	else
+	{
+		
+	}
+
+	return newRect;
+}
+
--- Experio/Source/Runtime/UI/RectTransform.h
+++ Experio/Source/Runtime/UI/RectTransform.h
@@ -1,13 +1,46 @@
 #pragma once
 #include "../Math/FRect.h"
+#include "../Math/FVector2.h"
+
+enum class EAnchorType : uint8_t
+{
+	Upper,
+	UpperLeft,
+	UpperCenter,
+	UpperRight,
+	Middle,
+	MiddleLeft,
+	MiddleCenter,
+	MiddleRight,
+	Bottom,
+	BottomLeft,
+	BottomCenter,
+	BottomRight,
+	Left,
+	Center,
+	Right,
+	Full
+};
 
 class RectTransform
 {
 public:
+	friend class GameObject;
+
 	RectTransform();
 	RectTransform(FVector2 min, FVector2 max);
+	RectTransform(FVector2 min, FVector2 max, FVector2 pivot);
+	RectTransform(FVector2 min, FVector2 max, FVector2 pivot, float rotation);
 
 	FRect rect;
+	float rotation;
+	FVector2 pivot;
+	EAnchorType anchorType;
 
-	// Add Dynamic Rect Design
+private:
+	static bool IsFillAnchorType(EAnchorType anchorType);
+
+	static FRect MergeRectTransform(FRect rect, FRect childToParent, EAnchorType childAnchorType, FVector2 screenDimensions);
+
+	static FRect MergeRectFull(FRect rect, FRect childToParent, FVector2 screenDimensions);
 };
