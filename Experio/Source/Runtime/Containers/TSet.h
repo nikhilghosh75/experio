90ff94b0030ee5d8b3009c07f0632b57ebcb2b38
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-03-15 12:49:38 -0400

    Created TSet

---
+++ Experio/Source/Runtime/Containers/TSet.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <functional>
+#include "TBinarySearchTree.h"
+
+template<class T, class Compare = std::less<T>>
+class TSet
+{
+	TBinarySearchTree<T> tree;
+
+public:
+	TSet()
+	{
+
+	}
+
+	TSet(std::initializer_list<T> list)
+	{
+		tree = TBinarySearchTree<T>(list);
+	}
+
+	size_t Count() const
+	{
+		return tree.Count();
+	}
+
+	void Insert(const T& item)
+	{
+		tree.Insert(item);
+	}
+
+	bool Exists(const T& item) const
+	{
+		return tree.Find() != nullptr;
+	}
+
+	const T* Find(const T& item) const
+	{
+		return tree.Find();
+	}
+
+	static TSet<T> Merge(TSet<T>& setOne, TSet<T>& setTwo)
+	{
+		TSet<T> set;
+		setOne.tree.ForEachPreOrder([&set](T& item) {set.Insert(set)});
+		setTwo.tree.ForEachPreOrder([&set](T& item) {set.Insert(set)});
+		return set;
+	}
+
+	void Remove(const T& item)
+	{
+		tree.Remove(item);
+	}
+};
