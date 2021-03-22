f61f9bec764e04f8747e9ad4c4ca78fc46b0f946
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-03-22 19:41:27 -0400

    Added ImageComponent to DefaultComponent.h

--- Experio/Source/Runtime/DefaultComponents.h
+++ Experio/Source/Runtime/DefaultComponents.h
@@ -5,4 +5,6 @@
 #include "Rendering/Components/Billboard.h"
 #include "Rendering/Components/MeshComponent.h"
 #include "Rendering/Particles/ParticleSystem.h"
+
 #include "UI/TextComponent.h"
+#include "UI/ImageComponent.h"
