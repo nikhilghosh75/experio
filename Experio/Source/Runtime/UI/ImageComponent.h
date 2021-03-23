b1f1ce338500a7391cfb21200b35bcf28b1c7807
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-03-22 20:57:03 -0400

    Created ImageComponent

---
+++ Experio/Source/Runtime/UI/ImageComponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../Rendering/Managers/TextureManager.h"
+#include "../Framework/Framework.h"
+
+class ImageComponent : public Component
+{
+	static const FRect imageUVRect;
+
+public:
+	ImageComponent();
+	ImageComponent(GameObject* gameObject);
+
+	TextureRef texture;
+
+	virtual void Start() override;
+
+	virtual void Update() override;
+
+	Shader* shader;
+};
