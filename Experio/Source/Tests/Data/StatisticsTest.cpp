c6765fa822213ff90f4a2bd670c0b8d2e2a1b84b
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-03-28 12:45:17 -0400

    Added WeightedAverage to StatisticsTest

--- Experio/Source/Tests/Data/StatisticsTest.cpp
+++ Experio/Source/Tests/Data/StatisticsTest.cpp
@@ -6,6 +6,7 @@ TEST_SUITE("Statistics", "Experio/Data");
 UNIT_TEST(TestStatisticsAverage, "Statistics")
 {
 	std::vector<float> vector = { 0, 4, 5, 6, 7, 17, 20 };
+	ASSERT_ALMOST_EQUAL(LStatistics::Sum(vector), 59);
 	ASSERT_ALMOST_EQUAL(LStatistics::Average(vector), 8.4286f);
 
 	TEST_END();
@@ -24,6 +25,15 @@ UNIT_TEST(TestStatisticsMode, "Statistics")
 {
 	std::vector<float> vector = { 1, 1, 2, 3, 4, 5, 6 };
 	ASSERT_ALMOST_EQUAL(LStatistics::Mode(vector), 1);
+
+	TEST_END();
+}
+
+UNIT_TEST(TestStatisticsWeightedAverage, "Statistics")
+{
+	std::vector<float> values = { 0.9, 0.8, 0.6, 0.4, 0.92, 0.85 };
+	std::vector<float> weights = { 10, 10, 20, 30, 15, 15 };
+	ASSERT_ALMOST_EQUAL(LStatistics::WeightedAverage(values, weights), 67.55);
 
 	TEST_END();
 }
