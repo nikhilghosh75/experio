72946bc7428a3ad2e2328a5e8943bcaa448dbc99
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-04-01 16:10:58 -0400

    Created Quadratic Regression

--- Experio/Source/Runtime/Data/Regression.h
+++ Experio/Source/Runtime/Data/Regression.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <vector>
 
+/// <summary>
+/// A series of regression types to calculate 
+/// </summary>
+
 class Regression
 {
 public:
@@ -36,5 +40,39 @@ public:
 	virtual float Get(float x) const override
 	{
 		return a * x + b;
+	}
+};
+
+// y = ax^2 + bx + c
+class QuadraticRegression : public Regression
+{
+public:
+	float a;
+	float b;
+	float c;
+
+	QuadraticRegression(const std::vector<float>& x, const std::vector<float>& y)
+	{
+		unsigned int n = x.size();
+		float sumX = 0;
+		float sumX2 = 0;
+		float sumX2Y = 0;
+		float sumXY = 0;
+		float sumX3 = 0;
+		float sumY = 0;
+		float sumX4 = 0;
+
+		for (size_t i = 0; i < x.size(); i++)
+		{
+			sumX += x[i];
+			sumX2 += x[i] * x[i];
+			sumX3 += x[i] * x[i] * x[i];
+			sumX4 += x[i] * x[i] * x[i] * x[i];
+			sumY += y[i];
+			sumXY += x[i] * y[i];
+			sumX2Y += x[i] * x[i] * y[i];
+		}
+
+		// ADD STUFF LATER
 	}
 };
