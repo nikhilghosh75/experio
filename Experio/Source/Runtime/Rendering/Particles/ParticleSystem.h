750db73bf3f4accaf7841344f943c1b2253f1a7c
DESKTOP-1N7COEK\debgh <extremewierdness.p@gmail.com>
2021-04-26 12:37:02 -0400

    Added Start and modifiers to ParticleSystem

--- Experio/Source/Runtime/Rendering/Particles/ParticleSystem.h
+++ Experio/Source/Runtime/Rendering/Particles/ParticleSystem.h
@@ -8,8 +8,17 @@ class ParticleSystem
 public:
 	std::string name;
 	uint32_t maxCount;
+	float defaultLifetime;
+
+	ParticleSystem();
+	ParticleSystem(const ParticleSystem& particleSystem);
 
 	size_t SizeOf() const;
 
+	SpawnModifier spawnModifier;
+
+	std::vector<StartParticleModifier*> startModifiers;
 	std::vector<ParticleModifier*> modifiers;
+
+	void Start(ParticleComponent& component);
 };
